f5427080d0c1e0ae8d41401534e291f0a68e4a33
Jesse Donat <donatj@gmail.com>
2019-06-21 16:21:30 -0500

    Add helper text

--- main.go
+++ main.go
@@ -4,9 +4,15 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+
+	"github.com/mattn/go-isatty"
 )
 
 func main() {
+	if isatty.IsTerminal(os.Stdin.Fd()) {
+		os.Stderr.WriteString("Input string and hit ^D when complete\n")
+	}
+
 	data, err := ioutil.ReadAll(os.Stdin)
 	if err != nil {
 		panic(err)
